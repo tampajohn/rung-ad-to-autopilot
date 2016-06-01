aaafb89c10fe8b0839e83e0feb3a68edce499992
John Adams <johnadams@jqatampa.com>
2016-06-01 19:19:53 -0400

    progress

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request-json');
+var mongoClient = require('mongodb').MongoClient;
 
 var client = request.createClient('https://api2.autopilothq.com/');
 client.headers["autopilotapikey"] = "6ad2dbded5cb44c19a5826784ba07889";
@@ -15,18 +16,18 @@ app.post('/autopilot', function(req, res) {
     console.log(JSON.stringify(req.body, null, 4));
     var payload = {};
     //Twitter
-    if (req.body.email && req.body.screen_name && req.body.name) {
-        console.log("Twitter post")
-        payload = {
-            "contact": { 
-                "Email": req.body.email,
-                "Twitter": req.body.screen_name,
-                "FirstName": req.body.name
-            }
-        };  
-    }
+    // if (req.body.email && req.body.screen_name && req.body.name) {
+    //     console.log("Twitter post")
+    //     payload = {
+    //         "contact": { 
+    //             "Email": req.body.email,
+    //             "Twitter": req.body.screen_name,
+    //             "FirstName": req.body.name
+    //         }
+    //     };  
+    // }
     //Maitre New User
-    else if (req.body.list_uuid && req.body.response == "new_registration") {
+    if (req.body.list_uuid && req.body.response == "new_registration") {
         console.log("Maitre post");
         payload = {
             "contact": {
@@ -42,16 +43,31 @@ app.post('/autopilot', function(req, res) {
         payload = {
             "contact": {
                 "FirstName": req.body.name,
-                "Email": req.body.email,
+                "Email": req.body.email,                
                 "_autopilot_list": "contactlist_A73207C3-05D3-4270-965B-9B19467F1AF8"       
             }
         }
+        mongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds021462.mlab.com:21462/rung-io', function(err,db) {
+           db.collection('beta-request').insert(payload, function(err, result) {
+               
+           })
+        });
     }
-    if (payload.contact && payload.contact.Email) {        
-        client.post('v1/contact', payload, function(err,httpResponse,body){ 
-            res.send("Added Maitre to autopilot");
-            res.end(); 
-        });   
+    if (payload.contact && payload.contact.Email) {
+        mongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds021462.mlab.com:21462/rung-io', function(err,db) {
+           db.collection('beta-request').insert(payload, function(err, result) {
+               if (!err) {
+                    client.post('v1/contact', payload, function(err,httpResponse,body){ 
+                        res.send("Added Maitre to autopilot");
+                        res.end(); 
+                    });   
+               } else {
+                   res.send("Didn't work");
+                   res.end();
+               }       
+           })
+        });        
+           
     } else {
         res.send("Nothing done");
         res.end();
